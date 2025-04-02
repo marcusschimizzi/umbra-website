db65ba0cc8c95d423fed91b9719f09123f5dc25b
Marcus Schimizzi <marcus@schimizzi.io>
2025-04-01 21:15:58 -0500

    update theme data

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -3,7 +3,9 @@ import type { LucideIcon } from "lucide-react";
 export interface ThemeColors {
   bg: string;
   secondary: string;
+  tertiary: string;
   text: string;
+  secondaryText: string;
   accent: string;
   syntax: string[];
 }
